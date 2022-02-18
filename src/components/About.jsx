7784eae102d47eb3d6bca9b9b3894797b16468a8
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-02-18 01:19:01 -0300

    Jueves 17 - About update
    Comienzo de página about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,27 @@
-import {Fragment,React} from 'react';
-
+import { Fragment, React } from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
-    return ( 
-<Fragment>
-    <div>Hola mundo desde About</div>
-</Fragment>
-     );
-}
- 
+  return (
+    <Fragment>
+      <section class="hero is-info is-fullheight">
+        <div class="hero-body">
+          <div class="tile is-ancestor is-vertical">
+              <div className="tile is-vertical is-12 box">
+                <p className="title has-text-black">Titulo</p>
+              </div>
+              <div className="tile is-vertical is-12 box">
+                <div className="tile is-parent">
+                    
+                </div>
+              </div>
+              
+              
+            </div>
+          </div>
+      </section>
+    </Fragment>
+  );
+};
+
 export default About;
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -9,10 +9,13 @@ const Index = () => {
           <div class="container has-text-centered">
             <div class="columns is-flex is-vcentered">
               <div class="column is-size-2">
-                <p>Rodrigo Romero</p>
+                <div className="block"></div>
+                <p class="is-size-4">Mi nombre es Rodrigo Romero, soy programador en entrenamiento, me gusta aprender nuevas tecnologías y aplicarlas en proyectos personales a modo de práctica.</p>
+                <div className="block"></div>
+                <p class="is-size-4">Para conocer más sobre mis experiencias y conocimientos en programación presiona en "Más información".</p>
                 <div class="mt-6">
                   <Link to="/about" class="button is-warning is-medium">
-                    Acerca de mí
+                    Mas información
                   </Link>
                 </div>
               </div>
