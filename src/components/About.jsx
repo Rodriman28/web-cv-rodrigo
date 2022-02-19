bd4bfc1e3d7e7e5ed74e27a2a1cb11b6b9e46543
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-02-19 20:41:39 -0300

    19/2/22
    Web finalizada, queda pendiente la adaptación a móviles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,21 +4,137 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <Fragment>
-      <section class="hero is-info is-fullheight">
+      <section class="hero background-about is-fullheight">
         <div class="hero-body">
           <div class="tile is-ancestor is-vertical">
-              <div className="tile is-vertical is-12 box">
-                <p className="title has-text-black">Titulo</p>
-              </div>
-              <div className="tile is-vertical is-12 box">
-                <div className="tile is-parent">
-                    
+            <div className="tile is-parent">
+              <div className="tile is-child is-parent">
+                <div className="tile">
+                  {" "}
+                  {/* Bloque del boton */}
+                  <div className="tile animation-5 invisible">
+                    <Link to="/" class="button is-dark has-text-white is-normal">
+                      Volver
+                    </Link>
+                  </div>
                 </div>
               </div>
-              
-              
+            </div>
+
+            <div className="tile is-parent is-12">
+              <div className="tile is-child is-9">
+                <div className="tile is-parent">
+                  <div className="tile is-child box invisible animation-1">
+                    {" "}
+                    {/* Acerca de mi */}
+                    <p className="title has-text-black">Acerca de mí</p>
+                    <p className="has-text-black">
+                      Si bien mi gusto por la informática comenzó desde niño, mi
+                      interés por la programación se desarrolló durante el
+                      bachillerato de informática, desde ahí me dediqué a crear
+                      proyectos pequeños como calculadoras, aplicaciones de
+                      gestión o simples interfaces para poner en practica cada
+                      cosa nueva que aprendía.
+                    </p>
+                    <p className="has-text-black">
+                      Soy una persona a la que le gusta proponerse desafíos con
+                      cada nueva tecnología que adquiere, desde crear un
+                      programa en varios lenguajes para comprobar cual es el mas
+                      adecuado, hasta realizar web apps con distintas bases de
+                      datos para comprender su implementación.
+                    </p>
+                    <p className="has-text-black"></p>
+                  </div>
+                </div>
+                <div className="tile">
+                  <div className="tile is-parent is-vertical">
+                    {" "}
+                    {/* Grupo de bloques de la izquierda*/}
+                    <div className="tile is-child box invisible animation-2">
+                      {" "}
+                      {/* Cursos realizados */}
+                      <p className="title has-text-black">Cursos realizados</p>
+                      <div className="content has-text-black">
+                        <ul>
+                          <li>Javascript moderno guía definitiva (Udemy)</li>
+                          <li>
+                            Crea una aplicación implementando MERN stack (Udemy)
+                          </li>
+                          <li>Bootstrap 5 curso completo (Udemy)</li>
+                          <li>Curso de HTML5 desde cero (Youtube/Soy Dalto)</li>
+                          <li>Curso de CSS desde cero (Youtube/Soy Dalto)</li>
+                          <li>
+                            Curos de JavaScript desde cero (Youtube/Soy Dalto)
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="tile is-child box invisible animation-3">
+                      {" "}
+                      {/* Experiencias y proyectos */}
+                      <p className="title has-text-black">
+                        Experiencias y Proyectos
+                      </p>
+                      <div className="content has-text-black">
+                        <ul>
+                          <li>
+                            Search Proyect -> Proyecto de fin de año del
+                            Bachillerato de Informática, realizado en Visual
+                            Basic
+                          </li>
+                          <li>
+                            Zero service app -> Aplicación web de gestión de
+                            servicios técnicos para Zero Informática, realizada
+                            en stack MERN
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="tile is-parent is-3 is-vertical">
+                    {" "}
+                    {/* Bloque vertical */}
+                    <div className="tile is-child box invisible animation-4">
+                      <p className="title has-text-black">
+                        Tecnologías conocidas
+                      </p>
+                      <div className="content has-text-black">
+                        <ul>
+                          <li>HTML</li>
+                          <li>CSS</li>
+                          <li>JavaScript</li>
+                          <li>MongoDB</li>
+                          <li>React</li>
+                          <li>Bootstrap</li>
+                          <li>Bulma</li>
+                          <li>Electrón</li>
+                          <li>Java</li>
+                          <li>C#</li>
+                          <li>MySQL</li>
+                          <li>Express</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="tile is-child">
+                <div className="tile is-parent">
+                  <div className="tile is-child invisible animation-5">
+                    <figure className="image">
+                      <img
+                        className="img-max"
+                        src="https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ"
+                        alt="Mountain"
+                      />
+                    </figure>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
       </section>
     </Fragment>
   );
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -4,27 +4,28 @@ import { Link } from "react-router-dom";
 const Index = () => {
   return (
     <Fragment>
-      <section class="hero is-info is-fullheight">
-        <div class="hero-body">
+      <section class="hero image-rodri is-fullheight">
+          <div class="hero-body">
           <div class="container has-text-centered">
             <div class="columns is-flex is-vcentered">
               <div class="column is-size-2">
                 <div className="block"></div>
-                <p class="is-size-4">Mi nombre es Rodrigo Romero, soy programador en entrenamiento, me gusta aprender nuevas tecnologías y aplicarlas en proyectos personales a modo de práctica.</p>
+                <p class="is-size-4 has-text-black animation-1 invisible">Mi nombre es Rodrigo Romero, soy programador en entrenamiento, me gusta aprender nuevas tecnologías y aplicarlas en proyectos personales a modo de práctica.</p>
                 <div className="block"></div>
-                <p class="is-size-4">Para conocer más sobre mis experiencias y conocimientos en programación presiona en "Más información".</p>
+                <p class="is-size-4 has-text-black animation-1 invisible">Para conocer más sobre mis experiencias y conocimientos en programación presiona en "Más información".</p>
                 <div class="mt-6">
-                  <Link to="/about" class="button is-warning is-medium">
+                  <Link to="/about" class="button is-dark is-medium has-text-white invisible animation-3">
                     Mas información
                   </Link>
                 </div>
               </div>
               <div class="column">
-                <img src="https://placekitten.com/640/360" alt="" />
+                
               </div>
             </div>
           </div>
         </div>
+        
       </section>
     </Fragment>
   );
